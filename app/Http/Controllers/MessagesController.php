a9eae3dfc2b4f8112ac160daef156704b3c0403d
lauorozco <lorozco@aulasamigas.com>
2018-04-06 17:07:35 -0500

    Display messages

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -7,10 +7,8 @@ use Illuminate\Http\Request;
 
 class MessagesController extends Controller
 {
-    public function show($id)
+    public function show(Message $message)
     {
-        $message = Message::find($id);
-
         return view ('messages.show', [
             'message' => $message,
         ]);
--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+	<h1 class="h3">Mensaje id: {{ $message->id }}</h1>
+    <img src="{{ $message->image }}" class="img-thumbnail">
+    <p class="card-text">
+    	{{ $message->content }}
+    	<small class="tex-view">{{ $message->created_at }}</small>
+    </p>
 @endsection
